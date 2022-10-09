b7b1f8579dd970d46f69ffd9b88da80bd9739e7a
Bartek <229300@edu.p.lodz.pl>
2022-10-09 12:08:48 +0200

    Add removing chars from string due to number parameter before .k

--- lab2/main.c
+++ lab2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int my_printf(char *format_string, char *param){
@@ -7,8 +8,37 @@ int my_printf(char *format_string, char *param){
     char arrayOutput[100];
     int helperFlag = 0;
     int arrayIndex = 0;
+    int numberToPrint = 0;
 
-	for(int i=0;i<strlen(format_string);i++){
+	// for(int i=0;i<strlen(format_string);i++){
+	// 	if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+	// 		i++;
+			
+	// 		for(int j = 0; j<strlen(param); j++) {
+	// 		char letter = param[j];
+			
+			
+	// 			if(letter >= 65 && letter <= 90) {
+	// 			   param[j] = letter + 32;
+	// 			}
+				
+	// 			if (letter >= 97 && letter <= 122) {
+	// 			   param[j] = letter - 32;
+	// 			}
+	// 		}
+			
+	// 		printf("%s",param);
+	// 	} 
+    //     else if ((format_string[i] == '#') && (format_string[i+1] == '.')){
+    //         i++;
+    //         helperFlag = 1;
+    //     }
+    //     else {
+    //         printf("%c", format_string[i]);
+	// }
+
+
+    	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
 			i++;
 			
@@ -26,15 +56,16 @@ int my_printf(char *format_string, char *param){
 			}
 			
 			printf("%s",param);
-		} if else ((format_string[i] == '#') && (format_string[i+1] == '.')){
+		}  else if ((format_string[i] == '#') && (format_string[i+1] == '.')){
             i++;
             helperFlag = 1;
         }
         else {
 			putchar(format_string[i]);
-	}
+        }
 
-    if(helperFlag == 1) {
+
+            if(helperFlag == 1) {
         char c = format_string[i];
 
         if(c >= 48 && c <= 57) {
@@ -42,12 +73,24 @@ int my_printf(char *format_string, char *param){
             arrayIndex++;
         }
 
-        if (c == "k") {
+        if (c == 'k') {
+            //tutaj usunąć K
             helperFlag = 0;
+
+            numberToPrint = atoi(arrayOutput);
         }
+
+
     }
 
-	puts("");
+    if(numberToPrint) {
+        for(int i = 0; i <= numberToPrint; i++) {
+            printf("%c", param[i]);
+        }
+        numberToPrint = 0;
+    }
+
+	}
 }
 
 int main(int argc, char *argv[]){
