be0135cd8c181a5f3769e357defaf41ea25e5347
Bartek <229300@edu.p.lodz.pl>
2022-10-09 12:28:50 +0200

    Add replacing size of letters in #.Xk module

--- lab2/main.c
+++ lab2/main.c
@@ -10,34 +10,6 @@ int my_printf(char *format_string, char *param){
     int arrayIndex = 0;
     int numberToPrint = 0;
 
-	// for(int i=0;i<strlen(format_string);i++){
-	// 	if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-	// 		i++;
-			
-	// 		for(int j = 0; j<strlen(param); j++) {
-	// 		char letter = param[j];
-			
-			
-	// 			if(letter >= 65 && letter <= 90) {
-	// 			   param[j] = letter + 32;
-	// 			}
-				
-	// 			if (letter >= 97 && letter <= 122) {
-	// 			   param[j] = letter - 32;
-	// 			}
-	// 		}
-			
-	// 		printf("%s",param);
-	// 	} 
-    //     else if ((format_string[i] == '#') && (format_string[i+1] == '.')){
-    //         i++;
-    //         helperFlag = 1;
-    //     }
-    //     else {
-    //         printf("%c", format_string[i]);
-	// }
-
-
     	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
 			i++;
@@ -69,7 +41,6 @@ int my_printf(char *format_string, char *param){
 			
         }
 
-
         if(helperFlag == 1) {
             char c = format_string[i];
 
@@ -92,8 +63,15 @@ int my_printf(char *format_string, char *param){
                 if(i >= paramLength) {
                     break;
                 }
-                
-                printf("%c", param[i]);
+
+                if(param[i] >= 65 && param[i] <= 90){
+                        printf("%c", param[i] + 32);
+                    }
+                    
+                if (param[i] >= 97 && param[i] <= 122){
+                        printf("%c", param[i] - 32);
+                    }
+
             }
             numberToPrint = 0;
         }
