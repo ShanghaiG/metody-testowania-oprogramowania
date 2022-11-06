f46e2687a61f56b1ce39e5c581fc5db699eb7996
ShanghaiG <75404684+ShanghaiG@users.noreply.github.com>
2022-11-06 11:39:26 +0100

    Add two functions to check if param is a number

--- lab4/main.js
+++ lab4/main.js
@@ -4,15 +4,31 @@ process.stdin.setEncoding('utf8');
 
 var lingeringLine = "";
 
-function my_printf(format_string,param){
-	for(var i=0;i<format_string.length;i++){
-		if((format_string.charAt(i) == '#') && (format_string.charAt(i+1) == 'k')){
-			process.stdout.write(param);
+function checkIfNumber (letter) {
+		return /^\d+$/.test(letter);
+}
+
+function myPrintf(formatString, param){
+	for(var i=0;i<formatString.length;i++){
+		if((formatString.charAt(i) == '#') && (formatString.charAt(i+1) == 'g')){
+			let newNumber = "";
+		
+			for(let letter of param) {
+				if(checkIfNumber(letter)) {
+					newNumber += letter;
+				}
+			}
+
+
+			process.stdout.write(newNumber);
+
 			i++;
-		}else{
-			process.stdout.write(format_string.charAt(i));
+		}
+		else{
+			process.stdout.write(formatString.charAt(i));
 		}
 	}
+
 	console.log("");
 }
 
@@ -22,7 +38,7 @@ process.stdin.on('data', function(chunk) {
 	lines[0] = lingeringLine + lines[0];
 	lingeringLine = lines.pop();
 	for(var i=0;i<lines.length;i++){
-		my_printf(lines[i],lines[i+1])
+		myPrintf(lines[i],lines[i+1])
 		i++;
 	}
 
