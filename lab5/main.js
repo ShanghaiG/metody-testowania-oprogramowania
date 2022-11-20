1bd86ec9b8293c5da74cd57fdf83112d92bed01f
ShanghaiG <75404684+ShanghaiG@users.noreply.github.com>
2022-11-20 10:22:03 +0100

    Add lab5 initial folder

---
+++ lab5/main.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var lingeringLine = "";
+
+function checkIfNumber (letter) {
+		return /^\d+$/.test(letter);
+}
+
+function reverseString (passedString) {
+	return passedString.split("").reverse().join("");
+}
+
+function myPrintf(formatString, param){
+	for(var i=0;i<formatString.length;i++){
+		if((formatString.charAt(i) == '#') && (formatString.charAt(i+1) == 'g')){
+			let newNumber = "";
+			
+			if(param){
+			for(let letter of param) {
+				if(checkIfNumber(letter)) {
+					newNumber += letter;
+				}
+			}
+			newNumber = reverseString(newNumber);
+			process.stdout.write(newNumber);
+
+			i++;
+		}
+		}
+		else{
+			process.stdout.write(formatString.charAt(i));
+		}
+	}
+
+	console.log("");
+}
+
+process.stdin.on('data', function(chunk) {
+	lines = chunk.split("\n");
+
+	lines[0] = lingeringLine + lines[0];
+	lingeringLine = lines.pop();
+	for(var i=0;i<lines.length;i++){
+		myPrintf(lines[i],lines[i+1])
+		i++;
+	}
+
+});
+
