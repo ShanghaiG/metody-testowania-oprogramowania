786a395a24d8382e5494875ce30590ce31d95801
ShanghaiG <75404684+ShanghaiG@users.noreply.github.com>
2022-11-20 11:27:43 +0100

    Start working on #Xg function

--- lab5/main.js
+++ lab5/main.js
@@ -10,26 +10,52 @@ function checkIfNumber (letter) {
 
 
 
-function () {
-
+function shiftNumber (singleNumber) {
+	if(singleNumber === 0) {
+		return 9;
+	} else {
+		return singleNumber-1;
+	}
 }
 
 function myPrintf(formatString, param){
 	for(var i=0;i<formatString.length;i++){
-		if((formatString.charAt(i) == '#') && (formatString.charAt(i+1) == 'g')){
-			let newNumber = "";
-			
-			if(param){
-			for(let letter of param) {
-				if(checkIfNumber(letter)) {
-					newNumber += letter;
-				}
-			}
-			newNumber = ;
-			process.stdout.write(newNumber);
 
-			i++;
-		}
+		if ((formatString.charAt(i) == "#") && (formatString.charAt(i+1) !== "g")) {
+			// console.log("zajrzalem");
+			let beforeGchars = "";
+			let j = i+1;
+			let finalShiftedNumber = "";
+			let singleNumberHolder = 0;
+
+
+			while(formatString.charAt(j) != "g") {
+				console.log("co tutaj", formatString.charAt(j));
+				beforeGchars += formatString.charAt(j);
+				j++;
+			}
+
+			if(param){
+				let shiftedParam = param;
+
+				if (shiftedParam.length > +beforeGchars) {
+					shiftedParam = shiftedParam.substring(0, beforeGchars);
+				}
+
+
+				for(let letter of shiftedParam) {
+					if(checkIfNumber(letter)) {
+						singleNumberHolder = shiftNumber(+letter);
+	
+						finalShiftedNumber += singleNumberHolder;
+					}
+				}
+				
+				process.stdout.write(finalShiftedNumber);
+	
+				i++;
+			}
+
 		}
 		else{
 			process.stdout.write(formatString.charAt(i));
